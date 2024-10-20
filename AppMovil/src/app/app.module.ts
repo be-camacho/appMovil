84374c241ec92f69647982c27652ec935df81246
be-camacho <be.camacho@duocuc.cl>
2024-10-19 22:23:12 -0300

    minor-changes

--- AppMovil/src/app/app.module.ts
+++ AppMovil/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { SharedModule } from 'src/app/modules/SharedModule/shared.module';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
--- AppMovil/src/environments/environment.prod.ts
+++ AppMovil/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig : {
+    apiKey: "AIzaSyCqyjrYSVarEOS6w-LhWDjQVpUlIpEQj68",
+    authDomain: "datamobileapp-b2ace.firebaseapp.com",
+    projectId: "datamobileapp-b2ace",
+    storageBucket: "datamobileapp-b2ace.appspot.com",
+    messagingSenderId: "20503300175",
+    appId: "1:20503300175:web:041f32b1b3e851156e423a"
+  }
 };
